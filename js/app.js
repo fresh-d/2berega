5b1913af137dc6b65a955916abe7a7257dc12a80
fresh-d <frodomogoni@gmail.com>
2013-09-04 12:03:50 +0300

    Удален tooltip из search-table.html

--- js/app.js
+++ js/app.js
@@ -363,21 +363,21 @@ $(function() {
 
 
 	/*Table overflow cells*/
-	var table = $("[data-table_overflow]");
-	var tr = table.find('tr:not(:eq(0))');
-
-	table.addClass('b-gradient_overflow');
-	tr.each(function(){
-		var td  = $(this).find('td:eq(5), td:eq(7), td:eq(8)');
-		td.each(function(){
-			var i_width = parseInt($(this).css('max-width')) || 74;
-			//console.log($(this).find('div').width() +'>='+ i_width);
-			if($(this).find('div').width() >= i_width){
-				var s_text = $(this).find('div').text();
-				$(this).find('div').addClass('overflow').html($('<div>').text(s_text));
-			}
-		});
-	});
+//	var table = $("[data-table_overflow]");
+//	var tr = table.find('tr:not(:eq(0))');
+//
+//	table.addClass('b-gradient_overflow');
+//	tr.each(function(){
+//		var td  = $(this).find('td:eq(5), td:eq(7), td:eq(8)');
+//		td.each(function(){
+//			var i_width = parseInt($(this).css('max-width')) || 74;
+//			//console.log($(this).find('div').width() +'>='+ i_width);
+//			if($(this).find('div').width() >= i_width){
+//				var s_text = $(this).find('div').text();
+//				$(this).find('div').addClass('overflow').html($('<div>').text(s_text));
+//			}
+//		});
+//	});
 
 	/*multiselect*/
 	$('[data-multi-select]').each(function(){
